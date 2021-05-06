5638377329c42c9f31dc55b07a1fbec74a352f28
Brian Musembi <brian.musembi@mths.ca>
2021-05-06 23:16:34 +0000

    added month.cpp

---
+++ month.cpp
@@ -0,0 +1,87 @@
+// Copyright (c) 2021 Brian Musembi All Rights Reserved
+//
+// Created by: Brian Musembi
+// Created on: 06 May 2021
+// This program tells the month of the year
+
+
+#include <iostream>
+
+int main() {
+    // this function tells the month of the year
+
+    int month;
+
+    // input
+    std::cout << "Enter the number of a month (ex 3 for March): ";
+    std::cin >> month;
+
+    // process
+    if (month == 1) {
+        // output
+        std::cout << "January.";
+
+    // process
+}   else if (month == 2) {
+        // output
+        std::cout << "February.";
+
+    // process
+}   else if (month == 3) {
+        // output
+        std::cout << "March.";
+
+    // process
+}   else if (month == 4) {
+        // output
+        std::cout << "April.";
+
+    // process
+}   else if (month == 5) {
+        // output
+        std::cout << "May.";
+
+    // process
+}   else if (month == 6) {
+        // output
+        std::cout << "June.";
+
+    // process
+}   else if (month == 7) {
+        // output
+        std::cout << "July.";
+
+    // process
+}   else if (month == 8) {
+        // output
+        std::cout << "August.";
+
+    // process
+}   else if (month == 9) {
+        // output
+        std::cout << "September.";
+
+    // process
+}   else if (month == 10) {
+        // output
+        std::cout << "October.";
+
+    // process
+}   else if (month == 11) {
+        // output
+        std::cout << "November.";
+
+    // process
+}   else if (month == 12) {
+        // output
+        std::cout << "December.";
+
+    // process
+}   else {
+        // output
+        std::cout << "" << std::endl;
+        std::cout << "Please input a number from 1-12!";}
+        std::cout << "" << std::endl;
+        std::cout << "" << std::endl;
+        std::cout << "Done" << std::endl;
+}
